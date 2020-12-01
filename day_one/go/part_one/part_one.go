c2aced03e3dbeebf0eff10a91bfba2c65a1338ea
michael <connellmp@gmail.com>
2020-12-01 14:11:15 -0800

    remove comments

--- day_one/go/part_one/part_one.go
+++ day_one/go/part_one/part_one.go
@@ -40,29 +40,3 @@ func checkList() int {
 func main() {
 	fmt.Println(checkList())
 }
-
-// You are scanning the numbers once to read them.
-
-// You read them into a slice of strings, then convert them into a slice of ints.
-// It is possible to either scan it directly to an int (and handle the error), or scan to a string,
-// and strconv it into an int (and handle the error), then make just the one []int.
-
-// Then you iterate over them all, and scan the entire list for both the first number and 2020-thatNumber.
-// You already know the first number is in the list because you're iterating over it, so that's unnecessary.
-
-// So if you remove that, you're iterating over the numbers and then scanning the entire list (potentially)
-// by calling valInList(2020-n, intNums).
-
-// Now, since you're both iterating the list and scanning the list, your solution is O(n2) in complexity.
-// That is, worst case, it will take time in proportion to the square of the length of the list.
-// This can be done in O(n) -- and the second problem in O(n2).
-
-// Because the input size is just 200 numbers, this isn't a big deal at and brute force is a ok,
-// especially in a programming competition. This is about knowing your data.
-
-// Doing it your way, if I had a list of []int, I might go something like:
-
-// for _,i := range intNums { for _,j := range intNums { if i + j == 2020 { return i * j } } }
-// That's a simple brute force O(n2) solution and decently quick to type without error.
-
-// for competition you might skip handling errors in exchange for speed
