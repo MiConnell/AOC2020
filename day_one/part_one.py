2a09f8a17b5c3fdcf2630c04890dfa8b3d3d2eda
michael <connellmp@gmail.com>
2020-11-30 21:24:23 -0800

    refactor and simplify

--- day_one/part_one.py
+++ day_one/part_one.py
@@ -1,13 +1,10 @@
 def calculate(s: str, goal: int) -> int:
-    nums = []
-    with open(s, 'r') as f:
-        for line in f.readlines():
-            line = int(line)
-            nums.append(line)
+    nums = [int(line) for line in open(s).readlines()]
     for n in nums:
         if goal - n in nums:
             return (goal - n) * n
     return 0
+
 
 if __name__ == "__main__":
     print(calculate('./blob.txt', 2020))
