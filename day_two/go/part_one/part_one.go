1b1fb185a8c24ea89e4a2ecf2b0a258db478791c
michael <connellmp@gmail.com>
2020-12-01 21:53:43 -0800

    day two solve part one Python

---
+++ day_two/go/part_one/part_one.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+)
+
+func checkList() int {
+	var intNums []int
+
+	f, err := os.Open("../blob.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		intText, err := strconv.Atoi(scanner.Text())
+		if err != nil {
+			log.Fatal(err)
+		}
+		intNums = append(intNums, intText)
+	}
+
+	for _, i := range intNums {
+		for _, j := range intNums {
+			if i+j == 2020 {
+				return i * j
+			}
+		}
+	}
+
+	return 0
+}
+
+func main() {
+	fmt.Println(checkList())
+}
---
+++ day_two/go/part_two/part_two.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"github.com/ernestosuarez/itertools"
+)
+
+
+func checkList() int {
+	var intNums []int
+
+	f, err := os.Open("../blob.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		intText, err := strconv.Atoi(scanner.Text())
+		if err != nil {
+			log.Fatal(err)
+		}
+		intNums = append(intNums, intText)
+	}
+
+	for i := range itertools.CombinationsInt(intNums, 3) {
+		if i[0] + i[1] + i[2] == 2020 {
+			return i[0] * i[1] * i[2]
+		}
+	}
+	return 0
+}
+
+func main() {
+	fmt.Println(checkList())
+}
