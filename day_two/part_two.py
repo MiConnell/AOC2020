479872987dec6fb73f3c627ff63d2b20e4ba95db
michael <connellmp@gmail.com>
2020-12-01 22:04:49 -0800

    format files

--- day_two/part_two.py
+++ day_two/part_two.py
@@ -1,19 +1,20 @@
 def password_validator(file: str) -> int:
-    with open(file, 'r') as f:
+    with open(file, "r") as f:
         total = 0
         for line in f.readlines():
             line = line.split()
             params = line[0]
-            first_index = int(params.split('-')[0]) - 1
-            second_index = int(params.split('-')[-1]) - 1
-            char = line[1].replace(':', '')
+            first_index = int(params.split("-")[0]) - 1
+            second_index = int(params.split("-")[-1]) - 1
+            char = line[1].replace(":", "")
             password = line[-1]
-            if password[first_index] == password[second_index]:
-                pass
-            elif char in (password[first_index], password[second_index]):
+            if (
+                char in (password[first_index], password[second_index])
+                and password[first_index] != password[second_index]
+            ):
                 total += 1
     return total
 
 
 if __name__ == "__main__":
-    print(password_validator('./blob.txt'))
+    print(password_validator("./blob.txt"))
