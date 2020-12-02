bbf4c5bb955ca6e824e707d2e75b89a2d29715de
michael <connellmp@gmail.com>
2020-12-01 22:01:37 -0800

    solve part two Python and prepare go

---
+++ day_two/part_two.py
@@ -0,0 +1,19 @@
+def password_validator(file: str) -> int:
+    with open(file, 'r') as f:
+        total = 0
+        for line in f.readlines():
+            line = line.split()
+            params = line[0]
+            first_index = int(params.split('-')[0]) - 1
+            second_index = int(params.split('-')[-1]) - 1
+            char = line[1].replace(':', '')
+            password = line[-1]
+            if password[first_index] == password[second_index]:
+                pass
+            elif char in (password[first_index], password[second_index]):
+                total += 1
+    return total
+
+
+if __name__ == "__main__":
+    print(password_validator('./blob.txt'))
