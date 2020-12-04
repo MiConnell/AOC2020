de024cb49aa5a8a2533adbf2c513f52d1a1e6756
michael <connellmp@gmail.com>
2020-12-03 21:38:01 -0800

    start part one

--- Day04/part01.py
+++ Day04/part01.py
@@ -1,20 +1,10 @@
-def tree_counter(file: str) -> int:
+def passport_validator(file: str) -> int:
     total = 0
     with open(file, "r") as f:
-        trees = [line.replace("\n", "") for line in f.readlines()]
-        x, y = 0, 0
-        x += 3
-        x %= len(trees[0])
-        y += 1
-        while y < len(trees):
-            if trees[y][x] == "#":
-                total += 1
-            x += 3
-            x %= len(trees[0])
-            y += 1
-
+        passports = [p.split("\n \n") for p in f.readlines()]
+        print(passports)
     return total
 
 
 if __name__ == "__main__":
-    print(tree_counter("./blob.txt"))
+    print(passport_validator("./blob.txt"))
