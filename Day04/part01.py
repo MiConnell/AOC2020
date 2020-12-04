4822132f0bdd7c5078e91c84a784cda417d86a72
michael <connellmp@gmail.com>
2020-12-04 12:08:38 -0800

    solve day 4 part 1 python

--- Day04/part01.py
+++ Day04/part01.py
@@ -1,8 +1,25 @@
+REQUIRED = {
+    "byr",
+    "iyr",
+    "eyr",
+    "hgt",
+    "hcl",
+    "ecl",
+    "pid",
+}
+
+
 def passport_validator(file: str) -> int:
     total = 0
     with open(file, "r") as f:
-        passports = [p.split("\n \n") for p in f.readlines()]
-        print(passports)
+        passports = f.read().split("\n\n")
+        for p in passports:
+            p = p.replace("\n", " ").strip()
+            check = [a.split(":")[0] for a in p.split(" ")]
+            if "cid" in check:
+                check.remove("cid")
+            if len(list(set(check))) > 6:
+                total += 1
     return total
 
 
