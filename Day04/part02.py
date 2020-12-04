5d075b535683ecea561e9adfc0eb5b00161ce1d7
michael <connellmp@gmail.com>
2020-12-04 12:31:30 -0800

    set up part two

--- Day04/part02.py
+++ Day04/part02.py
@@ -1,41 +1,31 @@
-file = "./blob.txt"
+REQUIRED = {
+    "byr",
+    "iyr",
+    "eyr",
+    "hgt",
+    "hcl",
+    "ecl",
+    "pid",
+}
 
 
-def _counter(file: str, x_shift: int, y_shift: int) -> int:
+def passport_validator(file: str) -> int:
+    total = 0
     with open(file, "r") as f:
-        total = 0
-        lines = [line.replace("\n", "") for line in f.readlines()]
-        x, y = 0, 0
-        x += x_shift
-        x %= len(lines[0])
-        y += y_shift
-        while y < len(lines):
-            if lines[y][x] == "#":
-                total += 1
-            x += x_shift
-            x %= len(lines[0])
-            y += y_shift
-
+        passports = f.read().split("\n\n")
+        for p in passports:
+            p = p.replace("\n", " ").strip()
+            check = [a.split(":") for a in p.split(" ")]
+            keys = []
+            values = []
+            passw_dict = {}
+            for c in check:
+                keys.append(c[0])
+                values.append(c[1])
+            for k, v in zip(keys, values):
+                passw_dict[k] = v
     return total
 
 
-def tree_counter(file: str) -> int:
-    return (
-        _counter(file, 1, 1)
-        * _counter(file, 3, 1)
-        * _counter(file, 5, 1)
-        * _counter(file, 7, 1)
-        * _counter(file, 1, 2)
-    )
-
-
 if __name__ == "__main__":
-    print(tree_counter(file))
-
-"""
-Right 1, down 1.
-Right 3, down 1. (This is the slope you already checked.)
-Right 5, down 1.
-Right 7, down 1.
-Right 1, down 2.
-"""
+    print(passport_validator("./blob.txt"))
