1fa05f969434b331e48610683c1a36ed8f286778
michael <connellmp@gmail.com>
2020-12-05 00:36:59 -0800

    start part 1

--- Day05/part01.py
+++ Day05/part01.py
@@ -1,27 +1,45 @@
-REQUIRED = {
-    "byr",
-    "iyr",
-    "eyr",
-    "hgt",
-    "hcl",
-    "ecl",
-    "pid",
-}
+from typing import List
 
 
-def passport_validator(file: str) -> int:
-    total = 0
+def bin_search(lst: List[int]) -> int:
+    left: int = 0
+    right = lst.index(lst[-1])
+    return left + (right - left) // 2
+
+
+def row_finder(row_string: str) -> int:
+    row_range = list(range(128))
+    rows = []
+    for s in row_string:
+        if s == "B":
+            row_range = row_range[bin_search(row_range) + 1 :]
+        elif s == "F":
+            row_range = row_range[: bin_search(row_range)]
+        rows.append(row_range)
+    return [r for r in rows if len(r) == 1][0][0]
+
+
+def column_finder(column_string: str) -> int:
+    column_range = list(range(8))
+    columns = []
+    for s in column_string:
+        if s == "R":
+            column_range = column_range[bin_search(column_range) + 1 :]
+        elif s == "L":
+            column_range = column_range[: bin_search(column_range)]
+        columns.append(column_range)
+    return [c for c in columns if len(c) == 1][0][0]
+
+
+def seat_checker(file: str) -> int:
     with open(file, "r") as f:
-        passports = f.read().split("\n\n")
-        for p in passports:
-            p = p.replace("\n", " ").strip()
-            check = [a.split(":")[0] for a in p.split(" ")]
-            if "cid" in check:
-                check.remove("cid")
-            if len(check) > 6:
-                total += 1
-    return total
+        total = [
+            (row_finder(options[:7]) * 8) + column_finder(options[-3:])
+            for options in f.readlines()
+        ]
+
+    return max(total)
 
 
 if __name__ == "__main__":
-    print(passport_validator("./blob.txt"))
+    print(seat_checker("./blob.txt"))
