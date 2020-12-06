a64e38f08816f99d9eed97f5f78954895e033056
michael <connellmp@gmail.com>
2020-12-05 23:17:41 -0800

    create franken-comprehension

--- Day06/part02.py
+++ Day06/part02.py
@@ -1,20 +1,18 @@
-from typing import Any
-from typing import List
-
-
-def counter(lst: List[Any]) -> int:
-    total = [(let, lst[0].count(let)) for let in lst[0] if lst[0].count(let) == lst[1]]
-    return len(set(total))
-
-
 def answer_checker(file: str) -> int:
     with open(file, "r") as f:
-        answers = [
-            [a.replace("\n", ""), len(a.split("\n"))]
-            for a in f.read().strip().split("\n\n")
-        ]
-        total = sum(counter(ans) for ans in answers)
-    return total
+        return sum(
+            len(
+                {
+                    (let, letter.count(let))  # type: ignore
+                    for let in letter  # type: ignore
+                    if letter.count(let) == value  # type: ignore
+                },
+            )
+            for (letter, value) in [
+                [a.replace("\n", ""), len(a.split("\n"))]
+                for a in f.read().strip().split("\n\n")
+            ]
+        )
 
 
 if __name__ == "__main__":
