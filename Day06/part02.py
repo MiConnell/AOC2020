14337079ca07bd163e89086286756dfb8f7468fd
michael <connellmp@gmail.com>
2020-12-05 22:50:02 -0800

    correct type hints

--- Day06/part02.py
+++ Day06/part02.py
@@ -1,10 +1,8 @@
-from typing import List
+from typing import Any, List
 
 
-def counter(lst: List[str]) -> int:
-    total = [
-        (let, lst[0].count(let)) for let in lst[0] if lst[0].count(let) == int(lst[1])
-    ]
+def counter(lst: List[Any]) -> int:
+    total = [(let, lst[0].count(let)) for let in lst[0] if lst[0].count(let) == lst[1]]
     return len(set(total))
 
 
@@ -13,7 +11,7 @@ def answer_checker(file: str) -> int:
         answers = []
         for a in f.read().strip().split("\n\n"):
             count = len(a.split("\n"))
-            answers.append([a.replace("\n", ""), str(count)])
+            answers.append([a.replace("\n", ""), count])
         total = sum(counter(ans) for ans in answers)
     return total
 
