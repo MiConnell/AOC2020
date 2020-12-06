f7404d67077a314fc207cc86358cdc52a9ba7855
michael <connellmp@gmail.com>
2020-12-05 20:13:47 -0800

    prepare part 1 and part 2

--- Day04/part01.go
+++ Day04/part01.go
@@ -5,8 +5,6 @@ import (
 	"fmt"
 	"log"
 	"os"
-	"strconv"
-	"strings"
 )
 
 func fileReader(file string) []string {
@@ -26,42 +24,23 @@ func fileReader(file string) []string {
 	return ret
 }
 
-func maxValue(lst []int64) int64 {
-	var maximum int64 = 0
-
-	for _, l := range lst {
-		if maximum <= l {
-			maximum = l
-		}
-	}
-
-	return maximum
-
-}
-
-func seatChecker(s []string) int64 {
-	var seats []int64
-
-	for _, s := range s {
-		s = strings.Replace(strings.Replace(
-			strings.Replace(
-				strings.Replace(
-					s, "F", "0", -1),
-				"B", "1", -1),
-			"L", "0", -1),
-			"R", "1", -1)
-		intS, err := strconv.ParseInt(s, 2, 64)
-
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		seats = append(seats, intS)
-	}
-
-	return maxValue(seats)
+func passportValidator(s []string) int64 {
+	return 0
 }
 
 func main() {
-	fmt.Println(seatChecker(fileReader("./blob.txt")))
+	fmt.Println(passportValidator(fileReader("./blob.txt")))
 }
+
+// def passport_validator(file: str) -> int:
+//     total = 0
+//     with open(file, "r") as f:
+//         passports = f.read().split("\n\n")
+//         for p in passports:
+//             p = p.replace("\n", " ").strip()
+//             check = [a.split(":")[0] for a in p.split(" ")]
+//             if "cid" in check:
+//                 check.remove("cid")
+//             if len(check) > 6:
+//                 total += 1
+//     return total
--- Day04/part02.go
+++ Day04/part02.go
@@ -9,46 +9,58 @@ import (
 
 var file string = "./blob.txt"
 
-func privateCounter(file string, xShift int, yShift int) int {
+func fileReader(file string) []string {
+
 	f, err := os.Open(file)
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer f.Close()
-
+	var ret []string
 	scanner := bufio.NewScanner(f)
-	total := 0
-	var trees []string
+
 	for scanner.Scan() {
-		text := scanner.Text()
-		trees = append(trees, text)
+		ret = append(ret, scanner.Text())
 	}
 
-	x := 0
-	y := 0
-	x += xShift
-	x %= len(trees[0])
-	y += yShift
-	for y < len(trees) {
-		if trees[y][x] == '#' {
-			total++
-		}
-		x += xShift
-		x %= len(trees[0])
-		y += yShift
-	}
-	return total
+	return ret
 }
 
-func treeCounter(file string) int {
+func valueChecker(s []string) int64 {
+	return 0
+}
 
-	return (privateCounter(file, 1, 1) *
-		privateCounter(file, 3, 1) *
-		privateCounter(file, 5, 1) *
-		privateCounter(file, 7, 1) *
-		privateCounter(file, 1, 2))
+func passportValidator(s []string) int64 {
+	return 0
 }
 
 func main() {
-	fmt.Println(treeCounter(file))
+	fmt.Println(passportValidator(fileReader(file)))
 }
+
+// def value_checker(dct: Dict[str, str]) -> bool:
+//     return (
+//         1920 <= int(dct["byr"]) <= 2002
+//         and 2010 <= int(dct["iyr"]) <= 2020
+//         and 2020 <= int(dct["eyr"]) <= 2030
+//         and (m1 := re.match(r"^(\d+)(cm|in)$", dct["hgt"]))
+//         and (
+//             (m1[2] == "cm" and 150 <= int(m1[1]) <= 193)
+//             or (m1[2] == "in" and 59 <= int(m1[1]) <= 76)
+//         )
+//         and re.match("^#[a-f0-9]{6}$", dct["hcl"])
+//         and dct["ecl"] in ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"]
+//         and re.match("^[0-9]{9}$", dct["pid"])
+//     )
+
+// def passport_validator(file: str) -> int:
+//     total = 0
+//     with open(file, "r") as f:
+//         passports = f.read().split("\n\n")
+//         for p in passports:
+//             p = p.replace("\n", " ").strip()
+//             check = [a.split(":") for a in p.split(" ")]
+//             passw_dict = {k: v for k, v in check}
+//             if passw_dict.keys() >= REQUIRED and value_checker(passw_dict):
+//                 total += 1
+//     return total
