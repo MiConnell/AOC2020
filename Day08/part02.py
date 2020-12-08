1739f80e10560f11590f31d987c334407925525a
michael <connellmp@gmail.com>
2020-12-08 11:38:46 -0800

    solve part 2 python

--- Day08/part02.py
+++ Day08/part02.py
@@ -1,12 +1,53 @@
 import os
+from typing import List
+from typing import Tuple
 
 file = os.path.join(os.path.dirname(__file__), "blob.txt")
 
+swap = {"nop": "jmp", "jmp": "nop"}
 
-def answer_checker(file: str) -> int:
+
+def inf_index(file: List[Tuple[str, str]], f: int) -> int:
+    acc = 0
+    ind = 0
+    seen_indexes = set()
+    lines = [line for line in file]
+    while ind not in seen_indexes and ind < len(lines):
+        seen_indexes.add(ind)
+        op, val = lines[ind][0], lines[ind][1]
+        if ind == f:
+            op = swap[op]
+        if op == "acc":
+            acc += eval(val)
+            ind += 1
+        elif op == "jmp":
+            ind += eval(val)
+        elif op == "nop":
+            ind += 1
+    if ind == len(lines):
+        return acc
+    else:
+        raise RuntimeError(seen_indexes)
+
+
+def solver(file: str) -> int:
     with open(file, "r") as f:
-        return 0
+        options = []
+        lines = [line.strip().split(" ") for line in f.readlines()]
+        for l in lines:
+            options.append((l[0], l[1]))
+        try:
+            inf_index(options, -1)
+        except Exception as e:
+            (seen_indexes,) = e.args
+    for i in seen_indexes:
+        if options[i][0] in ["jmp", "noc"]:
+            try:
+                return inf_index(options, i)
+            except Exception:
+                pass
+    return 0
 
 
 if __name__ == "__main__":
-    print(answer_checker(file))
+    print(solver(file))
--- Day08/tests/part02_test.py
+++ Day08/tests/part02_test.py
@@ -5,5 +5,5 @@ import part02
 test_file = os.path.join(os.path.dirname(__file__), "test_blob.txt")
 
 
-def test_part02():
-    assert part02.answer_checker(test_file) == 6
+def test_part01():
+    assert part02.solver(test_file) == 8
