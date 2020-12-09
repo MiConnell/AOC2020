33510c9a923990c271d9ddc1d2f2d253950ba9e5
michael <connellmp@gmail.com>
2020-12-09 09:31:43 -0800

    standardize function names

--- Day01/part01.py
+++ Day01/part01.py
@@ -3,8 +3,13 @@ import os
 file = os.path.join(os.path.dirname(__file__), "blob.txt")
 
 
-def calculate(s: str, goal: int) -> int:
-    nums = [int(line) for line in open(s).readlines()]
+def file_reader(file: str) -> str:
+    with open(file, "r") as f:
+        return f.read()
+
+
+def solver(s: str, goal: int) -> int:
+    nums = [int(line) for line in s.splitlines()]
     for n in nums:
         if goal - n in nums:
             return (goal - n) * n
@@ -12,4 +17,4 @@ def calculate(s: str, goal: int) -> int:
 
 
 if __name__ == "__main__":
-    print(calculate(file, 2020))
+    print(solver(file_reader(file), 2020))
--- Day01/part02.py
+++ Day01/part02.py
@@ -4,8 +4,13 @@ import os
 file = os.path.join(os.path.dirname(__file__), "blob.txt")
 
 
-def calculate(s: str, goal: int) -> int:
-    nums = sorted([int(line) for line in open(s).readlines()], reverse=True)
+def file_reader(file: str) -> str:
+    with open(file, "r") as f:
+        return f.read()
+
+
+def solver(s: str, goal: int) -> int:
+    nums = sorted([int(line) for line in s.splitlines()], reverse=True)
     for a, b, c in itertools.combinations(nums, 3):
         if a + b + c == goal:
             return a * b * c
@@ -13,4 +18,4 @@ def calculate(s: str, goal: int) -> int:
 
 
 if __name__ == "__main__":
-    print(calculate(file, 2020))
+    print(solver(file_reader(file), 2020))
