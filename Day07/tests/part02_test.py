ff03580504bebc1b28d3e3ce0b2d482fb9db052c
michael <connellmp@gmail.com>
2020-12-09 10:34:55 -0800

    correct test imports

--- Day07/tests/part02_test.py
+++ Day07/tests/part02_test.py
@@ -1,10 +1,10 @@
 import os
 
-from part01 import file_reader
-from part01 import solver
+from part02 import file_reader
+from part02 import solver
 
 test_file = os.path.join(os.path.dirname(__file__), "test_blob.txt")
 
 
-def test_part01():
+def test_part02():
     assert solver(file_reader(test_file)) == 6
