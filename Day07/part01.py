921ceeec1040ef3c36a4ea7d2e4861ab8515bae2
michael <connellmp@gmail.com>
2020-12-09 15:52:38 -0800

    create dictionary function

--- Day07/part01.py
+++ Day07/part01.py
@@ -1,12 +1,31 @@
 import os
 import re
+from collections import defaultdict
+from typing import DefaultDict
+from typing import List
 
 file = os.path.join(os.path.dirname(__file__), "blob.txt")
+
+PARENT = re.compile(r"^(\w+ \w+) bags contain (.+)$")
+CHILD = re.compile(r"^(\d+) (\w+ \w+)")
 
 
 def file_reader(file: str) -> str:
     with open(file, "r") as f:
         return f.read()
+
+
+def build_dictionary(s: str) -> DefaultDict[str, List[str]]:
+    parents = defaultdict(list)
+    for line in s.splitlines():
+        line_match = PARENT.match(line)
+        assert line_match, line
+        parent = line_match[1]
+        children = line_match[2]
+
+        for _, child in CHILD.findall(children):
+            parents[child].append(parent)
+    return parents
 
 
 def solver(s: str) -> int:
