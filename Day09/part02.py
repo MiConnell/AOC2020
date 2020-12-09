6398b3428e8d8499ca146417f45d474950a2f4ad
michael <connellmp@gmail.com>
2020-12-08 23:35:09 -0800

    solve part 2 python

--- Day09/part02.py
+++ Day09/part02.py
@@ -1,7 +1,8 @@
-import itertools
 import os
 
 file = os.path.join(os.path.dirname(__file__), "blob.txt")
+
+INVALID = 1721308972
 
 
 def file_reader(file: str) -> str:
@@ -9,20 +10,23 @@ def file_reader(file: str) -> str:
         return f.read()
 
 
-def solver(blob: str, preamble: int) -> int:
-    b = blob.splitlines()
+def solver(blob: str) -> int:
+    b = [int(b) for b in blob.splitlines()]
     start = 0
-    while b[preamble] != b[-1]:
-        checklist = [
-            int(num[0]) + int(num[1])
-            for num in itertools.combinations(b[start:preamble], 2)
-        ]
-        if (goal := int(b[preamble])) not in checklist:
-            return goal
-        start += 1
-        preamble += 1
-    return 0
+    end = 0
+    goal = INVALID
+    current = b[0]
+    while True:
+        if current < goal:
+            end += 1
+            current += b[end]
+        elif current > goal:
+            current -= b[start]
+            start += 1
+        else:
+            lst = b[start : end + 1]
+            return min(lst) + max(lst)
 
 
 if __name__ == "__main__":
-    print(solver(file_reader(file), 25))
+    print(solver(file_reader(file)))
