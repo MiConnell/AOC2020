ac4e4745acfb6bffbf4a2ce8b5e760bc3167fb7f
michael <connellmp@gmail.com>
2020-12-09 21:38:43 -0800

    solve part one python

--- Day10/part01.py
+++ Day10/part01.py
@@ -1,4 +1,3 @@
-import itertools
 import os
 
 file = os.path.join(os.path.dirname(__file__), "blob.txt")
@@ -9,19 +8,21 @@ def file_reader(file: str) -> str:
         return f.read()
 
 
-def solver(s: str, preamble: int) -> int:
-    b = s.splitlines()
-    start = 0
-    while b[preamble] != b[-1]:
-        checklist = [
-            int(f) + int(s) for f, s in itertools.combinations(b[start:preamble], 2)
-        ]
-        if (goal := int(b[preamble])) not in checklist:
-            return goal
-        start += 1
-        preamble += 1
+def solver(s: str) -> int:
+    adapters = sorted([int(n) for n in s.splitlines()])
+    current_heh = 0
+    total = {1: 0, 2: 0, 3: 1}
+    for jolty in adapters:
+        total[jolty - current_heh] += 1
+        current_heh = jolty
+    if total[1] != 0 and total[2] != 0 and total[3] != 0:
+        return total[1] * total[2] * total[3]
+    elif total[1] == 0:
+        return total[2] * total[3]
+    elif total[2] == 0:
+        return total[1] * total[3]
     return 0
 
 
 if __name__ == "__main__":
-    print(solver(file_reader(file), 25))
+    print(solver(file_reader(file)))
