3679970018700e89eb26ad6e562325bbc1ca2918
michael <connellmp@gmail.com>
2020-12-13 00:32:53 -0800

    solve part 1

--- Day13/part01.py
+++ Day13/part01.py
@@ -1,9 +1,6 @@
 import os
-from typing import Dict
 
 file = os.path.join(os.path.dirname(__file__), "blob.txt")
-
-directions: Dict[str, int] = {"N": 0, "E": 0, "S": 0, "W": 0}
 
 
 def file_reader(file: str) -> str:
@@ -12,9 +9,18 @@ def file_reader(file: str) -> str:
 
 
 def solver(s: str) -> int:
-    for line in s.splitlines():
-        print(line)
-    return 0
+    timestamp = int(s.splitlines()[0])
+    final_timestamp = timestamp
+    bus_ids = [int(bus) for bus in s.splitlines()[1].split(",") if bus != "x"]
+    max_mod = 0
+    curr_bus = 0
+    for bus in bus_ids:
+        if (mod := timestamp % bus) > max_mod:
+            max_mod = mod
+            curr_bus = bus
+    while final_timestamp % curr_bus != 0:
+        final_timestamp += 1
+    return (final_timestamp - timestamp) * curr_bus
 
 
 if __name__ == "__main__":
