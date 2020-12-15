5efc2ab8c6f914e64d0cd5955c6bfb9fad0dbcdd
michael <connellmp@gmail.com>
2020-12-14 20:57:02 -0800

    continue day 14

--- Day14/part01.py
+++ Day14/part01.py
@@ -9,18 +9,9 @@ def file_reader(file: str) -> str:
 
 
 def solver(s: str) -> int:
-    timestamp = int(s.splitlines()[0])
-    final_timestamp = timestamp
-    bus_ids = [int(bus) for bus in s.splitlines()[1].split(",") if bus != "x"]
-    max_mod = 0
-    curr_bus = 0
-    for bus in bus_ids:
-        if (mod := timestamp % bus) > max_mod:
-            max_mod = mod
-            curr_bus = bus
-    while final_timestamp % curr_bus != 0:
-        final_timestamp += 1
-    return (final_timestamp - timestamp) * curr_bus
+    for line in s.splitlines():
+        return int(line)
+    return 0
 
 
 if __name__ == "__main__":
